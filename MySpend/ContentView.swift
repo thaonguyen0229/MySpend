b16bb2d3e783fb7bdf4c6b54b89c2f1ed6b113d0
Amanda Pham <thaonguyen0229@gmail.com>
2023-05-31 11:32:54 -0400

    Initial Commit

---
+++ MySpend/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MySpend
+//
+//  Created by Amanda Pham on 2023-05-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MySpend/MySpendApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySpendApp.swift
+//  MySpend
+//
+//  Created by Amanda Pham on 2023-05-31.
+//
+
+import SwiftUI
+
+@main
+struct MySpendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
